2cf8ea05d4bbdd96e1378d68180e1f25dbd42d62
adrene <adrenemcintosh@yahoo.com>
2017-02-27 01:27:08 +0000

    inital commit

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "HuffLePuff"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:root@localhost/lab5"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
+
+db = SQLAlchemy(app)
+
+# Flask-Login login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+app.config.from_object(__name__)
+from app import views
